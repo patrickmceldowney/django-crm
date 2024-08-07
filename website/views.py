45d66b3eb3250ed1ec93bf9b89709123d6ed5fc4
--replace-all <ptmceldowney@gmail.com>
2024-08-06 22:54:00 -0500

    adding registration form

--- website/views.py
+++ website/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 # Create your views here.
 def home(request):
-	
-	return render(request, 'home.html', {})
+	# Check to see if the user is logging in
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		# Authenticate
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, 'You have been logged in')
+			return redirect('home')
+		else:
+			messages.error(request, 'There was an error logging in. Please try again...')
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {})
 
 def logout_user(request):
-	pass
+	logout(request)
+	messages.success(request, 'You have been logged out')
+	return redirect('home')
+
+def register_user(request):
+	return render(request, 'register.html', {})
