690658a200795da529789415aaaf601e6cdc2180
--replace-all <ptmceldowney@gmail.com>
2024-09-18 15:47:17 -0500

    Update record

--- website/views.py
+++ website/views.py
@@ -84,3 +84,18 @@ def add_record(request):
 	else:
 		messages.error(request, 'You must be logged in...')
 		return redirect('home')
+
+
+def update_record(request, pk):
+	if request.user.is_authenticated:	
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record updated successfully.")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form': form})
+	else:
+		messages.error(request, "You must be logged in to do that.")
+		return redirect('home')
